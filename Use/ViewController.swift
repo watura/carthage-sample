6e79adc3d626c3777316be3f20966df493b7df56
Wataru NISHIMOTO <watura@g91.org>
2018-10-03 19:31:03 +0900

    Import HogeLibrary

---
+++ Use/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Use
+//
+//  Created by Wataru Nishimoto on 2018/10/3.
+//  Copyright © 2018 Wataru Nishimoto. All rights reserved.
+//
+
+import UIKit
+import HogeLibrary
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
