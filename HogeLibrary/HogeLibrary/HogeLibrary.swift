c897ce0558a83baf856a4903b39c4bf8957ac448
Wataru NISHIMOTO <watura@g91.org>
2018-10-03 19:14:05 +0900

    HogeLibrary

---
+++ HogeLibrary/HogeLibrary/HogeLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  HogeLibrary.swift
+//  HogeLibrary
+//
+//  Created by Wataru Nishimoto on 2018/10/3.
+//  Copyright © 2018 Wataru Nishimoto. All rights reserved.
+//
+
+import Foundation
+
+public class HogeLibrary {
+    public func hoge() {
+        print("Hoge")
+    }
+}
