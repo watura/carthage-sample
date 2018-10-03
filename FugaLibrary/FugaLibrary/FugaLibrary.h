a530fe880f520b7095014eb11dddce1212a0673d
Wataru NISHIMOTO <watura@g91.org>
2018-10-03 19:34:06 +0900

    Fuga Library

---
+++ FugaLibrary/FugaLibrary/FugaLibrary.h
@@ -0,0 +1,19 @@
+//
+//  FugaLibrary.h
+//  FugaLibrary
+//
+//  Created by Wataru Nishimoto on 2018/10/3.
+//  Copyright © 2018 Wataru Nishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FugaLibrary.
+FOUNDATION_EXPORT double FugaLibraryVersionNumber;
+
+//! Project version string for FugaLibrary.
+FOUNDATION_EXPORT const unsigned char FugaLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FugaLibrary/PublicHeader.h>
+
+
---
+++ FugaLibrary/FugaLibrary/FugaLibrary.swift
@@ -0,0 +1,16 @@
+//
+//  FugaLibrary.swift
+//  FugaLibrary
+//
+//  Created by Wataru Nishimoto on 2018/10/3.
+//  Copyright © 2018 Wataru Nishimoto. All rights reserved.
+//
+
+import Foundation
+public class FugaLibrary {
+    public init() {}
+
+    public func fuga() {
+        print("Fuga")
+    }
+}
